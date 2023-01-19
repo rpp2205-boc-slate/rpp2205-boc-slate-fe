bad6421396750d15929fb406723327c6a5a6b113
Sijia Tao <taoshika127@gmail.com>
2023-01-18 20:47:36 -0800

    initial creation of skeleton

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export default function App(props) {
+
+  return(
+    <>
+      <h1> Welcome to GamerCity! </h1>
+    </>
+  );
+
+}
