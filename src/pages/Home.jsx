34008eaea67e00393087ed6807b357b42aa89df7
viviennema <98420335+viviennema@users.noreply.github.com>
2023-01-23 19:05:43 -0800

    add basic Navbar to all pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,16 @@ import { useState, useEffect } from "react";
 // import { StreamChat } from 'stream-chat';
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Navigation from "../components/Navigation And Authentication/Navigation.jsx";
 
 export default function Home(props) {
 
   return(
     <>
       <h1> placeholder for home page... </h1>
-      <Link to="/login">Login</Link>
-      <Link to="/signup">Signup</Link>
+      <Navigation />
+      {/* <Link to="/login">Login</Link>
+      <Link to="/signup">SignUp</Link> */}
     </>
   );
 
