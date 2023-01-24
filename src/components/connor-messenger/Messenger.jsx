671e503aa2b1a3ca6e672175176b90218ddacc8f
Connor Thurston <connor.thurston1@gmail.com>
2023-01-23 19:43:00 -0700

    initial chat setup

---
+++ src/components/connor-messenger/Messenger.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { StreamChat } from 'stream-chat';
+import axios from "axios";
+
+
+export default function Messenger(props) {
+
+  return(
+    <>
+      <h1> Welcome to GamerCity! </h1>
+    </>
+  );
+
+}
