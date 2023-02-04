b0572e68c3e188d15263288fbd30ed7dcec46a95
Connor Thurston <connor.thurston1@gmail.com>
2023-02-04 11:08:36 -0700

    added friend request buttons and styling, only using dummy data currently, need to add username to friend req array objects

--- src/pages/FriendRequest.jsx
+++ src/pages/FriendRequest.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 
 function FriendRequestList(props) {
   const [showRequests, setShowRequests] = useState(false);
+  let testData = [{'userId': 7, 'username': 'Jaoquin'}, {'userId': 8, 'username': 'Emmanuel'}, {'userId': 9, 'username': 'Diego'}]
 
   // if (props.isAuthenticated && props.user) {
     const friendRequests = props.user.received_req_from;
     console.log('friend requests', Array.isArray(friendRequests))
     if (Array.isArray(friendRequests) && props.chatOpen) {
-      if (!friendRequests.length) {
+      if (friendRequests.length) {
         return (
           <div>
             <p>You have no pending friend requests...</p>
@@ -16,10 +17,14 @@ function FriendRequestList(props) {
       } else {
         return (
         <div>
-          <p>{friendRequests.length} Friends requests Pending</p>
-            <ul>
-              {friendRequests.map((request) => (
-                <li key={request.name}>{request.name}</li>
+          <p className="friend-request-list">{testData.length} Friends Requests Pending</p>
+            <ul className="friend-request-list">
+              {testData.map((request) => (
+                <li key={request.userId}>
+                  {request.username} wants to be your friend
+                  <button>Add Friend</button>
+                  <button>Reject Request</button>
+                </li>
               ))}
             </ul>
         </div>
