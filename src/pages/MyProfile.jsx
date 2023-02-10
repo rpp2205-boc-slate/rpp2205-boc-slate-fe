78220bb7d09f2c482df78a174639ced15d6ad84a
mattdailey173 <mattdailey173@gmail.com>
2023-02-09 20:02:59 -0600

    pulling

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -15,9 +15,9 @@ function MyProfile(props) {
     <div className="profile">
       <Navigation />
       <Profile selfId={selfId} selfProfile={selfProfile}/>
-      {types.map((t) => (
+      <div className="car-div">{types.map((t) => (
         <Carousel type={t} fav={selfProfile} fri={selfProfile}/>
-      ))}
+      ))}</div>
     </div>
   );
 }
